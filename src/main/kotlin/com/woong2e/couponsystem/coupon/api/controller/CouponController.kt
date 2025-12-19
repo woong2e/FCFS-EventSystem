065ced80667bff6146704235fdc520bcb70e440b
신재웅 (JaeWoong Shin) <tlswodnd4316@gmail.com>
2025-12-19 17:19:29 +0900

    [feat]: 응답구조를 통일한다 (#3)

--- src/main/kotlin/com/woong2e/couponsystem/coupon/api/controller/CouponController.kt
+++ src/main/kotlin/com/woong2e/couponsystem/coupon/api/controller/CouponController.kt
@@ -5,7 +5,8 @@ import com.woong2e.couponsystem.coupon.api.request.CouponIssueRequest
 import com.woong2e.couponsystem.coupon.application.response.CouponIssueResponse
 import com.woong2e.couponsystem.coupon.application.response.CouponResponse
 import com.woong2e.couponsystem.coupon.application.service.CouponService
-import org.springframework.http.HttpStatus
+import com.woong2e.couponsystem.global.response.ApiResponse
+import com.woong2e.couponsystem.global.response.status.SuccessStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -19,17 +20,14 @@ class CouponController(
 ) {
 
     @PostMapping
-    fun create(@RequestBody request: CouponCreateRequest): ResponseEntity<CouponResponse> {
+    fun create(@RequestBody request: CouponCreateRequest): ResponseEntity<ApiResponse<CouponResponse>> {
         val response = couponService.create(request)
-        return ResponseEntity
-            .status(HttpStatus.CREATED)
-            .body(response)
+        return ApiResponse.onSuccess(SuccessStatus.CREATED, response)
     }
 
     @PostMapping("/issue")
-    fun issue(@RequestBody request: CouponIssueRequest): ResponseEntity<CouponIssueResponse> {
+    fun issue(@RequestBody request: CouponIssueRequest): ResponseEntity<ApiResponse<CouponIssueResponse>> {
         val response = couponService.issue(request.couponId, request.userId)
-        return ResponseEntity
-            .ok(response)
+        return ApiResponse.onSuccess(SuccessStatus.OK, response)
     }
 }
