7d5d38401c5cde3640b25dd1268c75b6b9283382
신재웅 (JaeWoong Shin) <tlswodnd4316@gmail.com>
2026-01-02 18:49:32 +0900

    [test]: 테스트용 RedissonClient는 Mock으로 제공한다 (#9)

--- src/test/kotlin/com/woong2e/couponsystem/infra/redis/TestRedissonConfig.kt
+++ src/test/kotlin/com/woong2e/couponsystem/infra/redis/TestRedissonConfig.kt
@@ -4,9 +4,9 @@ import org.mockito.Mockito
 import org.redisson.api.RedissonClient
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Profile
+import org.springframework.test.context.ActiveProfiles
 
-@Profile("test")
+@ActiveProfiles("test")
 @TestConfiguration
 class TestRedissonConfig {
 
