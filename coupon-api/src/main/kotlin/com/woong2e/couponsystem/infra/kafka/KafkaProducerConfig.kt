8ccd714d4cf036b5c8c9920b1597f4c5b5e9cfc8
신재웅 (JaeWoong Shin) <tlswodnd4316@gmail.com>
2026-01-06 00:43:27 +0900

    [fix]: 적절한 JsonSerializer import하도록 수정한다 (#11)

--- coupon-api/src/main/kotlin/com/woong2e/couponsystem/infra/kafka/KafkaProducerConfig.kt
+++ coupon-api/src/main/kotlin/com/woong2e/couponsystem/infra/kafka/KafkaProducerConfig.kt
@@ -1,15 +1,14 @@
 package main.kotlin.com.woong2e.couponsystem.infra.kafka
 
-import com.fasterxml.jackson.databind.JsonSerializer
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.serializer.JsonSerializer
 
 @Configuration
 class KafkaProducerConfig(
