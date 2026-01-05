0d9bbf7093c09a2c01f5a1acc62721dc07c5fd5b
신재웅 (JaeWoong Shin) <tlswodnd4316@gmail.com>
2026-01-06 00:21:04 +0900

    [build]: SpringBoot 버전을 안정 버전으로 다운그레이드 한다 (#11)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.2.21" apply false
-    kotlin("plugin.spring") version "2.2.21" apply false
-    kotlin("plugin.jpa") version "2.2.21" apply false
-    id("org.springframework.boot") version "4.0.0" apply false
+    kotlin("jvm") version "1.9.25" apply false
+    kotlin("plugin.spring") version "1.9.25" apply false
+    kotlin("plugin.jpa") version "1.9.25" apply false
+    id("org.springframework.boot") version "3.4.1" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
 }
 
@@ -21,11 +21,14 @@ allprojects {
 }
 
 subprojects {
-    // 플러그인 적용 (ID 문자열 사용)
+    // 하위 모듈에 플러그인 적용
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
 
-    // Java Toolchain 설정
+    // Java 21 설정
     configure<JavaPluginExtension> {
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(21))
--- coupon-consumer/build.gradle.kts
+++ coupon-consumer/build.gradle.kts
@@ -1,29 +1,30 @@
-plugins {
-    id("org.springframework.boot")
-    id("io.spring.dependency-management")
-    kotlin("plugin.jpa")
-}
-
 dependencies {
+    // Kotlin & Core
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
 
+    // Spring Boot Web & Monitoring
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.micrometer:micrometer-registry-prometheus")
 
+    // Kafka
     implementation("org.springframework.kafka:spring-kafka")
 
+    // DB & JPA
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
 
+    // Redis
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
+    // Utils
     implementation("com.github.f4b6a3:ulid-creator:5.2.2")
 
+    // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
