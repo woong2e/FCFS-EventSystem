c497dc45d23636f8f4b049b879e42e68eaa30bb4
신재웅 (JaeWoong Shin) <tlswodnd4316@gmail.com>
2026-01-05 19:30:27 +0900

    [feat]: Kafka Producer 설정파일을 등록한다 (#11)

---
+++ coupon-api/src/main/kotlin/com/woong2e/couponsystem/infra/kafka/KafkaProducerConfig.kt
@@ -0,0 +1,31 @@
+package main.kotlin.com.woong2e.couponsystem.infra.kafka
+
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.kafka.common.serialization.LongSerializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+
+@Configuration
+class KafkaProducerConfig {
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, Any> {
+        val configProps = HashMap<String, Any>()
+        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "kafka:29092"
+
+        configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+
+        configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = LongSerializer::class.java
+
+        return DefaultKafkaProducerFactory(configProps)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, Any> {
+        return KafkaTemplate(producerFactory())
+    }
+}
