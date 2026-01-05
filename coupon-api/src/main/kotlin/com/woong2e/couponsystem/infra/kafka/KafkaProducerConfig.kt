f413eea37bd8665c182dc0acf40a545cdcbf1d2a
신재웅 (JaeWoong Shin) <tlswodnd4316@gmail.com>
2026-01-05 20:29:32 +0900

    [fix]: Json 직렬화, 역직렬화 도구를 버전에 맞게 수정한다 (#11)

--- coupon-api/src/main/kotlin/com/woong2e/couponsystem/infra/kafka/KafkaProducerConfig.kt
+++ coupon-api/src/main/kotlin/com/woong2e/couponsystem/infra/kafka/KafkaProducerConfig.kt
@@ -2,24 +2,28 @@ package main.kotlin.com.woong2e.couponsystem.infra.kafka
 
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
-import org.apache.kafka.common.serialization.LongSerializer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer
 
 @Configuration
-class KafkaProducerConfig {
+class KafkaProducerConfig(
+    @param:Value("\${spring.kafka.bootstrap-servers}")
+    private val bootstrapServers: String
+) {
 
     @Bean
     fun producerFactory(): ProducerFactory<String, Any> {
         val configProps = HashMap<String, Any>()
-        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "kafka:29092"
+        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
 
         configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
 
-        configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = LongSerializer::class.java
+        configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JacksonJsonSerializer::class.java
 
         return DefaultKafkaProducerFactory(configProps)
     }
--- coupon-consumer/src/main/kotlin/com/woong2e/couponsystem/infra/kafka/KafkaConsumerConfig.kt
+++ coupon-consumer/src/main/kotlin/com/woong2e/couponsystem/infra/kafka/KafkaConsumerConfig.kt
@@ -2,25 +2,31 @@ package main.kotlin.com.woong2e.couponsystem.infra.kafka
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.kafka.common.serialization.LongDeserializer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.support.serializer.JacksonJsonDeserializer
 
 @EnableKafka
 @Configuration
-class KafkaConsumerConfig {
+class KafkaConsumerConfig(
+    @param:Value("\${spring.kafka.bootstrap-servers}")
+    private val bootstrapServers: String
+) {
 
     @Bean
     fun consumerFactory(): ConsumerFactory<String, Any> {
         val props = HashMap<String, Any>()
-        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "kafka:29092"
+        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
+
         props[ConsumerConfig.GROUP_ID_CONFIG] = "coupon-group"
+
         props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-        props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = LongDeserializer::class.java
+        props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JacksonJsonDeserializer::class.java
 
         return DefaultKafkaConsumerFactory(props)
     }
