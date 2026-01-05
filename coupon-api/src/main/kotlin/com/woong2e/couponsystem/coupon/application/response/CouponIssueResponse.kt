d6a8aacb112cf3888cd9ec0a4c2204005351f7f5
신재웅 (JaeWoong Shin) <tlswodnd4316@gmail.com>
2026-01-05 18:55:45 +0900

    [feat]: 동기, 비동기 처리 방식의 응답에 대한 팩토리 메서드를 추가한다 (#11)

--- coupon-api/src/main/kotlin/com/woong2e/couponsystem/coupon/application/response/CouponIssueResponse.kt
+++ coupon-api/src/main/kotlin/com/woong2e/couponsystem/coupon/application/response/CouponIssueResponse.kt
@@ -3,6 +3,25 @@ package main.kotlin.com.woong2e.couponsystem.coupon.application.response
 import java.util.UUID
 
 data class CouponIssueResponse(
-    val result: String = "SUCCESS",
-    val issuedCouponId: UUID? = null
-)
+    val isSuccess: Boolean = true,
+    val issuedCouponId: UUID? = null,
+    val message: String? = null
+) {
+    companion object {
+        fun asyncIssued(): CouponIssueResponse {
+            return CouponIssueResponse(
+                isSuccess = true,
+                issuedCouponId = null,
+                message = "쿠폰 발급 요청이 대기열에 추가되었습니다. 잠시 후 보관함을 확인해주세요."
+            )
+        }
+
+        fun syncIssued(id: UUID): CouponIssueResponse {
+            return CouponIssueResponse(
+                isSuccess = true,
+                issuedCouponId = id,
+                message = "쿠폰 발급이 완료되었습니다."
+            )
+        }
+    }
+}
