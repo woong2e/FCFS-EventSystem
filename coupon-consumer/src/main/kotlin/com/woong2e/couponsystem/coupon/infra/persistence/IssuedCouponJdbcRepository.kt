bb2081ce10800b5cb3d82ccfe9474b56d6cf8e8a
신재웅 (JaeWoong Shin) <tlswodnd4316@gmail.com>
2026-01-13 01:28:15 +0900

    [fix]: 컬럼 데이터 타입에 맞게 PreparedStatement를 변경한다  (#11)

--- coupon-consumer/src/main/kotlin/com/woong2e/couponsystem/coupon/infra/persistence/IssuedCouponJdbcRepository.kt
+++ coupon-consumer/src/main/kotlin/com/woong2e/couponsystem/coupon/infra/persistence/IssuedCouponJdbcRepository.kt
@@ -4,9 +4,11 @@ import main.kotlin.com.woong2e.couponsystem.coupon.domain.entity.IssuedCoupon
 import main.kotlin.com.woong2e.couponsystem.coupon.domain.repository.IssuedCouponBatchRepository
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
+import java.nio.ByteBuffer
 import java.sql.PreparedStatement
 import java.sql.Timestamp
 import java.time.LocalDateTime
+import java.util.*
 
 @Repository
 class IssuedCouponJdbcRepository(
@@ -15,22 +17,33 @@ class IssuedCouponJdbcRepository(
 
     override fun batchInsert(coupons: List<IssuedCoupon>) {
         val sql = """
-            INSERT INTO issued_coupons (coupon_id, user_id, status, created_at, updated_at)
+            INSERT INTO issued_coupons (id, coupon_id, user_id, status, issued_at)
             VALUES (?, ?, ?, ?, ?)
         """.trimIndent()
+
+        val now = Timestamp.valueOf(LocalDateTime.now())
 
         jdbcTemplate.batchUpdate(
             sql,
             coupons,
-            coupons.size,
-            { ps: PreparedStatement, coupon: IssuedCoupon ->
-                ps.setString(1, coupon.couponId.toString())
-                ps.setLong(2, coupon.userId)
-                ps.setString(3, "ISSUED")
-                val now = Timestamp.valueOf(LocalDateTime.now())
-                ps.setTimestamp(4, now)
-                ps.setTimestamp(5, now)
-            }
-        )
+            coupons.size
+        ) { ps: PreparedStatement, coupon: IssuedCoupon ->
+            ps.setBytes(1, uuidToBytes(coupon.id))
+
+            ps.setBytes(2, uuidToBytes(coupon.couponId))
+
+            ps.setLong(3, coupon.userId)
+
+            ps.setString(4, coupon.status)
+
+            ps.setTimestamp(5, now)
+        }
+    }
+
+    private fun uuidToBytes(uuid: UUID): ByteArray {
+        val bb = ByteBuffer.wrap(ByteArray(16))
+        bb.putLong(uuid.mostSignificantBits)
+        bb.putLong(uuid.leastSignificantBits)
+        return bb.array()
     }
 }
